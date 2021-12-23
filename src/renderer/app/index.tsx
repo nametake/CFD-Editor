b88ee366c7495f602c739c3d6eb77466545cc1ee
nametake <nametake.kyarabuki@gmail.com>
2021-12-24 00:49:34 +0900

    Format index.tsx

--- src/renderer/app/index.tsx
+++ src/renderer/app/index.tsx
@@ -1,10 +1,11 @@
-import styled from '@emotion/styled';
 import React from 'react';
 
-const Txt = styled.div`
-  display: block;
+import styled from '@emotion/styled';
+
+const Text = styled.div`
+  color: pink;
 `;
 
 export const App = function App(): JSX.Element {
-  return <Txt>Hello world</Txt>;
+  return <Text>Hello world</Text>;
 };
