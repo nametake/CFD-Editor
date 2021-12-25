4337f428bce692de2f1397c5570cd8309bf7a4ae
nametake <nametake.kyarabuki@gmail.com>
2021-12-25 18:11:34 +0900

    Added storybook and babel

---
+++ .storybook/main.ts
@@ -0,0 +1,34 @@
+import path from 'path';
+import { StorybookConfig } from '@storybook/core-common';
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  core: {
+    builder: 'webpack5',
+  },
+  webpackFinal: async (config) => {
+    // https://github.com/storybookjs/storybook/issues/13145
+    const emotionReactEleven = path.dirname(
+      require.resolve('@emotion/react/package.json')
+    );
+    const emotionStyledEleven = path.dirname(
+      require.resolve('@emotion/styled/package.json')
+    );
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve?.alias,
+          '@emotion/core': emotionReactEleven,
+          '@emotion/styled': emotionStyledEleven,
+          'emotion-theming': emotionReactEleven,
+        },
+      },
+    };
+  },
+  typescript: { reactDocgen: false },
+};
+
+module.exports = config;
