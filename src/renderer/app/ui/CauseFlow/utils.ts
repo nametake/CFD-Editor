5ec562f1369da255a15904e92fbe3f6fbbc6e5d0
nametake <nametake.kyarabuki@gmail.com>
2021-12-28 10:15:26 +0900

    Removed map and filter ChangeNode

--- src/renderer/app/ui/CauseFlow/utils.ts
+++ src/renderer/app/ui/CauseFlow/utils.ts
@@ -1,52 +1,6 @@
-import { NodeChange } from 'react-flow-renderer';
-
 import { ElkNode } from 'elkjs';
 
-import { assertUnreachable } from '@/utils/assert';
-
 import { Node } from './types';
-
-export const mapChangeNode =
-  (changeNodes: NodeChange[]) =>
-    (node: Node): Node => {
-      const changeNode = changeNodes.find((cn) => node.id === cn.id);
-      switch (changeNode?.type) {
-        case 'dimensions':
-          return {
-            ...node,
-            position: {
-              x: changeNode.position?.x ?? 0,
-              y: changeNode.position?.y ?? 0,
-            },
-            width: changeNode.dimensions?.width,
-            height: changeNode.dimensions?.height,
-          };
-        case 'select':
-        case 'remove':
-        case undefined:
-          return node;
-        default: {
-          return assertUnreachable(changeNode);
-        }
-      }
-    };
-
-export const filterChangeNode =
-  (changeNodes: NodeChange[]) =>
-    (node: Node): boolean => {
-      const changeNode = changeNodes.find((cn) => node.id === cn.id);
-      switch (changeNode?.type) {
-        case 'remove':
-          return false;
-        case 'dimensions':
-        case 'select':
-        case undefined:
-          return true;
-        default: {
-          return assertUnreachable(changeNode);
-        }
-      }
-    };
 
 const mapNodeToElkNode =
   (nodes: Node[]) =>
