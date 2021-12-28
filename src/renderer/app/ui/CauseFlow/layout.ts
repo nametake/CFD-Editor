7a308aefba428920205383a17701e132477a53ca
nametake <nametake.kyarabuki@gmail.com>
2021-12-28 12:49:46 +0900

    Defined filter nodes

--- src/renderer/app/ui/CauseFlow/layout.ts
+++ src/renderer/app/ui/CauseFlow/layout.ts
@@ -1,4 +1,31 @@
-import { Node } from '.';
+import { CauseNodeType, ElementNodeType, Node, ResultNodeType } from '.';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const layoutNodes = (_nodes: Node[]): Node[] => [];
+type CauseNodeWithElements = CauseNodeType & {
+  elements: ElementNodeType[];
+};
+
+const makeElementNode = (nodes: Node[], parentNode: Node): ElementNodeType[] =>
+  nodes
+    .filter((node): node is ElementNodeType => node.type === 'element')
+    .filter((node) => node.parentNode, parentNode.id);
+
+const makeCauseNode = (nodes: Node[]): CauseNodeWithElements[] =>
+  nodes
+    .filter((node): node is CauseNodeType => node.type === 'cause')
+    .map<CauseNodeWithElements>((node) => ({
+      ...node,
+      elements: makeElementNode(nodes, node),
+    }));
+
+const makeResultNode = (nodes: Node[]): ResultNodeType[] =>
+  nodes.filter((node): node is ResultNodeType => node.type === 'result');
+
+export const layoutCauseNode = (
+  causeNodes: CauseNodeWithElements[]
+): CauseNodeWithElements[] => causeNodes;
+
+export const layoutNodes = (nodes: Node[]): Node[] => {
+  const causeNodes = makeCauseNode(nodes);
+  const resultNodes = makeResultNode(nodes);
+  return [...causeNodes, ...resultNodes];
+};
