e23bcaf5231896934f88cd537b915d63edafb2dc
nametake <nametake.kyarabuki@gmail.com>
2021-12-29 23:46:55 +0900

    Added DecisionTable and story

---
+++ src/renderer/app/ui/DecisionTable/index.stories.tsx
@@ -0,0 +1,22 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+/* eslint-enable */
+
+import { DecisionTable } from './index';
+
+// eslint-disable-next-line import/no-default-export
+export default {
+  title: 'ui/DecisionTable',
+  component: DecisionTable,
+} as ComponentMeta<typeof DecisionTable>;
+
+/* eslint-disable react/jsx-props-no-spreading */
+const Template: ComponentStory<typeof DecisionTable> = function Template() {
+  return <DecisionTable />;
+};
+/* eslint-enable */
+
+export const Default = Template.bind({});
+Default.args = {};
---
+++ src/renderer/app/ui/DecisionTable/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const DecisionTable = function DecisionTable(): JSX.Element {
+  return <div />;
+};
