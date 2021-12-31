79b50c6292bdbd7448fe5f15bc46eb30f2550577
nametake <nametake.kyarabuki@gmail.com>
2021-12-31 09:11:33 +0900

    Defined makeCondition

---
+++ src/renderer/app/ui/DecisionTable/utils.ts
@@ -0,0 +1,10 @@
+import { Condition } from '@/app/types';
+
+import { CellType } from './types';
+
+export const makeCondition = (grid: CellType[][]): Condition => {
+  // eslint-disable-next-line no-console
+  console.log(grid);
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+  return {} as any;
+};
