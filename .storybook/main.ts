4c1bf2e596f4183dab8526bbdee397b3709eec15
nametake <nametake.kyarabuki@gmail.com>
2022-01-02 00:08:18 +0900

    Added first view

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,11 @@ import path from 'path';
 import { StorybookConfig } from '@storybook/core-common';
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: [
+    '../src/renderer/app/ui/MainView/index.stories.tsx',
+    '../src/**/*.stories.mdx',
+    '../src/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   core: {
     builder: 'webpack5',
