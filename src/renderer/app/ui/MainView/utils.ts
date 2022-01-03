3c7711914bc77780170a8c60339e8b9f77c8e475
nametake <nametake.kyarabuki@gmail.com>
2022-01-03 16:22:11 +0900

    Implemented mergeRules

--- src/renderer/app/ui/MainView/utils.ts
+++ src/renderer/app/ui/MainView/utils.ts
@@ -1,4 +1,10 @@
 import { Edge, Node, Rule } from '@/app/types';
+import {
+  CellType,
+  MAIN_COLUMN,
+  STUB_COLUMN,
+  makeId,
+} from '@/app/ui/DecisionTable';
 import { assertUnreachable } from '@/app/utils/assert';
 
 export const makeRules = (
@@ -49,4 +55,57 @@ export const makeRules = (
     default:
       return assertUnreachable(currentNode);
   }
+};
+
+export const mergeRules = (grid: CellType[][], rules: Rule[]): CellType[][] => {
+  const newGrid = grid.map((row) => row.slice(0, 3));
+  const headerRows = grid.reduce<number[]>((prev, row, index) => {
+    if (row[MAIN_COLUMN].value.type !== 'TITLE') return prev;
+    return [...prev, index];
+  }, []);
+
+  // TODO remove magic number
+  const actionHeaderRow = headerRows[1];
+  rules.forEach((rule) => {
+    newGrid.forEach((row, i) => {
+      const isConditionHeader = i === 0 && row[0].value.type === 'TITLE';
+      const isActionHeader = i !== 0 && row[0].value.type === 'TITLE';
+      if (isConditionHeader) {
+        newGrid[i] = [
+          ...row,
+          { value: { type: 'TITLE', value: `${i + 1}` }, readOnly: true },
+        ];
+        return;
+      }
+      if (isActionHeader) {
+        newGrid[i] = [...row, { value: { type: 'EMPTY' }, readOnly: true }];
+        return;
+      }
+
+      const stubId = makeId({ row: i, col: STUB_COLUMN });
+      if (actionHeaderRow <= i) {
+        if (rule.conditionStubIds.find((id) => id === stubId)) {
+          newGrid[i] = [
+            ...row,
+            { value: { type: 'CONDITION_RULE', value: 'yes' } },
+          ];
+        }
+        newGrid[i] = [
+          ...row,
+          { value: { type: 'CONDITION_RULE', value: 'no' } },
+        ];
+        return;
+      }
+      if (stubId === rule.actionId) {
+        newGrid[i] = [
+          ...row,
+          { value: { type: 'CONDITION_RULE', value: 'yes' } },
+        ];
+        return;
+      }
+      newGrid[i] = [...row, { value: { type: 'CONDITION_RULE', value: 'no' } }];
+    });
+  });
+
+  return newGrid;
 };
