7785df5e84d38351235c5bcfb5b17f4562c5f68c
nametake <nametake.kyarabuki@gmail.com>
2022-01-18 00:28:15 +0900

    Moved creating node logic to reducer

--- src/renderer/app/pages/Main/state/action.ts
+++ src/renderer/app/pages/Main/state/action.ts
@@ -6,11 +6,7 @@ export type MainAction =
   | { type: 'CHANGED_CELLS'; payload: { changes: CellsChangedArgs } }
   | { type: 'CHANGED_NODES'; payload: { changes: NodeChange[] } }
   | { type: 'ADDED_CONNECTION'; payload: { connection: Connection } }
+  | { type: 'CLICK_REMOVE_EDGE'; payload: { id: string } }
   | { type: 'GRID/CLICK_ADD_CONDITION_ROW' }
   | { type: 'GRID/CLICK_ADD_ACTION_ROW' }
-  | { type: 'GRID/CLICK_REMOVE_ROW'; payload: { row: number } }
-  | { type: 'CLICK_ADD_ROW_TOP_BUTTON'; payload: { row: number } }
-  | { type: 'CLICK_REMOVE_EDGE'; payload: { id: string } }
-  | { type: 'CLICK_ADD_ROW_BOTTOM_BUTTON'; payload: { row: number } }
-  | { type: 'CLICK_REMOVE_ROW_BUTTON'; payload: { row: number } }
-  | { type: 'REMOVE_CONDITION_ROW' };
+  | { type: 'GRID/CLICK_REMOVE_ROW'; payload: { row: number } };
--- src/renderer/app/pages/Main/state/reducer.ts
+++ src/renderer/app/pages/Main/state/reducer.ts
@@ -10,39 +10,15 @@ import { Node as NodeUtils } from '@/app/utils/node';
 import { MainAction } from './action';
 import { MainState, emptyActionRow, emptyConditionRow } from './state';
 
-const merge = (prevNodes: Node[], newNodes: Node[]): Node[] =>
-  newNodes.map((newNode) => {
-    const prevNode = prevNodes.find((node) => node.id === newNode.id);
-    return {
-      ...prevNode,
-      ...newNode,
-      style: {
-        ...prevNode?.style,
-        ...newNode.style,
-      },
-    };
-  });
-
 const actionReducer: Reducer<MainState, MainAction> = (
   prev: MainState,
   action: MainAction
 ): MainState => {
   switch (action.type) {
     case 'CHANGED_CELLS': {
-      const grid = Grid.applyCellsChanges(prev.grid, action.payload.changes);
-
-      const conditions = Grid.toConditions(grid);
-      const conditionNodes = NodeUtils.fromConditions(conditions);
-
-      const actions = Grid.toActions(grid);
-      const resultNodes = NodeUtils.fromActions(actions);
-
-      const nodes = [...conditionNodes, ...resultNodes];
-
       return {
         ...prev,
-        grid,
-        nodes: merge(prev.nodes, nodes),
+        grid: Grid.applyCellsChanges(prev.grid, action.payload.changes),
       };
     }
     case 'CHANGED_NODES': {
@@ -89,43 +65,39 @@ const actionReducer: Reducer<MainState, MainAction> = (
         grid: [...prev.grid.slice(0, row), ...prev.grid.slice(row + 1, end)],
       };
     }
-    case 'CLICK_ADD_ROW_TOP_BUTTON': {
-      const { row } = action.payload;
-      const end = prev.grid.length;
-      return {
-        ...prev,
-        grid: [
-          ...prev.grid.slice(0, row),
-          emptyConditionRow,
-          ...prev.grid.slice(row, end),
-        ],
-      };
-    }
-    case 'CLICK_ADD_ROW_BOTTOM_BUTTON': {
-      const { row } = action.payload;
-      const end = prev.grid.length;
-      return {
-        ...prev,
-        grid: [
-          ...prev.grid.slice(0, row + 1),
-          emptyActionRow,
-          ...prev.grid.slice(row + 1, end),
-        ],
-      };
-    }
-    case 'CLICK_REMOVE_ROW_BUTTON': {
-      const { row } = action.payload;
-      const end = prev.grid.length;
-      return {
-        ...prev,
-        grid: [...prev.grid.slice(0, row), ...prev.grid.slice(row + 1, end)],
-      };
-    }
-    case 'REMOVE_CONDITION_ROW':
-      return prev;
     default:
       return assertUnreachable(action);
   }
+};
+
+const merge = (prevNodes: Node[], newNodes: Node[]): Node[] =>
+  newNodes.map((newNode) => {
+    const prevNode = prevNodes.find((node) => node.id === newNode.id);
+    return {
+      ...prevNode,
+      ...newNode,
+      style: {
+        ...prevNode?.style,
+        ...newNode.style,
+      },
+    };
+  });
+
+const nodesReducer: Reducer<MainState, MainAction> = (
+  state: MainState
+): MainState => {
+  const conditions = Grid.toConditions(state.grid);
+  const conditionNodes = NodeUtils.fromConditions(conditions);
+
+  const actions = Grid.toActions(state.grid);
+  const resultNodes = NodeUtils.fromActions(actions);
+
+  const nodes = [...conditionNodes, ...resultNodes];
+
+  return {
+    ...state,
+    nodes: merge(state.nodes, nodes),
+  };
 };
 
 const rulesReducer: Reducer<MainState, MainAction> = (
@@ -144,7 +116,7 @@ export const reducer: Reducer<MainState, MainAction> = (
   prev: MainState,
   action: MainAction
 ): MainState =>
-  [actionReducer, rulesReducer, layoutReducer].reduce(
+  [actionReducer, nodesReducer, rulesReducer, layoutReducer].reduce(
     (prevState, fn) => fn(prevState, action),
     prev
   );
