7718b669237d1da04824862b8f33f71eea8c6ae5
nametake <nametake.kyarabuki@gmail.com>
2022-01-23 16:22:14 +0900

    Addded setStyle story

--- src/renderer/app/ui/CauseFlow/index.stories.tsx
+++ src/renderer/app/ui/CauseFlow/index.stories.tsx
@@ -6,7 +6,10 @@ import { ComponentMeta, ComponentStory } from '@storybook/react';
 /* eslint-enable */
 
 import { Node } from '@/app/types';
+import { causeLabelStyle, causeNodeStyle } from '@/app/ui/CauseNode';
+import { elementNodeStyle } from '@/app/ui/ElementNode';
 import { layoutNodes } from '@/app/utils/layouts';
+import { setStyle } from '@/app/utils/node/setStyle';
 
 import { CauseFlow } from './CauseFlow';
 import { mapStyle } from './utils';
@@ -19,30 +22,24 @@ export default {
 } as ComponentMeta<typeof CauseFlow>;
 
 /* eslint-disable react/jsx-props-no-spreading */
-const Template: ComponentStory<typeof CauseFlow> = function Template({
-  nodes: argsNodes,
-  edges: argsEdges,
-  ...args
-}) {
-  const [nodes, setNodes] = useState(argsNodes);
-  const sort = useCallback((newNodes: Node[]) => {
-    setNodes(newNodes);
-  }, []);
-  return (
-    <CauseFlow
-      {...args}
-      nodes={nodes}
-      edges={argsEdges}
-      style={{ width: '1024', height: '1024px' }}
-      onNodesChange={useCallback(
-        (changeNodes: NodeChange[]) => {
-          sort(layoutNodes(applyNodeChanges(changeNodes, nodes).map(mapStyle)));
-        },
-        [nodes, sort]
-      )}
-    />
-  );
-};
+const TemplateWithLayout: ComponentStory<typeof CauseFlow> =
+  function TemplateWithLayout({ nodes: argsNodes, edges: argsEdges, ...args }) {
+    const [nodes, setNodes] = useState(argsNodes);
+    return (
+      <CauseFlow
+        {...args}
+        nodes={nodes}
+        edges={argsEdges}
+        style={{ width: '1024px', height: '1024px' }}
+        onNodesChange={useCallback(
+          (changeNodes: NodeChange[]) => {
+            setNodes(layoutNodes(applyNodeChanges(changeNodes, nodes)));
+          },
+          [nodes]
+        )}
+      />
+    );
+  };
 /* eslint-enable */
 
 const defaultNodes: Node[] = [
@@ -152,7 +149,7 @@ const defaultNodes: Node[] = [
   },
 ];
 
-export const Default = Template.bind({});
+export const Default = TemplateWithLayout.bind({});
 Default.args = {
   nodes: defaultNodes.map(mapStyle),
   edges: [
@@ -165,4 +162,74 @@ Default.args = {
     { id: 'c1-e4_r3', source: 'c1-e4', target: 'r3', type: 'removable' },
     { id: 'c2-e2_r3', source: 'c2-e2', target: 'r3', type: 'removable' },
   ],
+};
+
+/* eslint-disable react/jsx-props-no-spreading */
+const Template: ComponentStory<typeof CauseFlow> = function Template({
+  nodes: argsNodes,
+  edges: argsEdges,
+  ...args
+}) {
+  const [nodes, setNodes] = useState(argsNodes);
+  return (
+    <CauseFlow
+      {...args}
+      nodes={nodes}
+      edges={argsEdges}
+      style={{ width: '1024px', height: '1024px' }}
+      onNodesChange={useCallback(
+        (changeNodes: NodeChange[]) => {
+          setNodes(applyNodeChanges(changeNodes, nodes));
+        },
+        [nodes]
+      )}
+    />
+  );
+};
+/* eslint-enable */
+
+export const CauseNodeWithElement = Template.bind({});
+CauseNodeWithElement.args = {
+  nodes: setStyle(
+    [
+      {
+        id: 'c1',
+        type: 'cause',
+        data: {
+          label: {
+            text: 'Cause 1',
+          },
+        },
+        position: { x: 0, y: 0 },
+      },
+      {
+        id: 'c1-e1',
+        parentNode: 'c1',
+        type: 'element',
+        data: { label: 'Cause 1 Element 1' },
+        position: { x: 0, y: 0 },
+      },
+      {
+        id: 'c1-e2',
+        parentNode: 'c1',
+        type: 'element',
+        data: { label: 'Cause 1 Element 2' },
+        position: { x: 0, y: 0 },
+      },
+      {
+        id: 'c1-e3',
+        parentNode: 'c1',
+        type: 'element',
+        data: { label: 'Cause 1 Element 3' },
+        position: { x: 0, y: 0 },
+      },
+    ],
+    {
+      nodeStyles: {
+        causeNodeStyle,
+        causeNodeLabelStyle: causeLabelStyle,
+        elementNodeStyle,
+      },
+    }
+  ),
 };
