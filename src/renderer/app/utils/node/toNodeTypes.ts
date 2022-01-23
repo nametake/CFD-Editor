e2fc914f5e3a91cea66b21e5c74e672b0d5304e1
nametake <nametake.kyarabuki@gmail.com>
2022-01-23 14:55:24 +0900

    Added toNodeTypes.ts

---
+++ src/renderer/app/utils/node/toNodeTypes.ts
@@ -0,0 +1,28 @@
+import {
+  CauseNodeType,
+  ElementNodeType,
+  Node,
+  ResultNodeType,
+} from '@/app/types';
+
+export type CauseNodeWithElements = CauseNodeType & {
+  elements: ElementNodeType[];
+};
+
+const toElementNode = (nodes: Node[], parentNode: Node): ElementNodeType[] =>
+  nodes
+    .filter((node): node is ElementNodeType => node.type === 'element')
+    .filter((node) => node.parentNode === parentNode.id);
+
+export const toCauseNodeWithElements = (
+  nodes: Node[]
+): CauseNodeWithElements[] =>
+  nodes
+    .filter((node): node is CauseNodeType => node.type === 'cause')
+    .map<CauseNodeWithElements>((node) => ({
+      ...node,
+      elements: toElementNode(nodes, node),
+    }));
+
+export const toResultNode = (nodes: Node[]): ResultNodeType[] =>
+  nodes.filter((node): node is ResultNodeType => node.type === 'result');
