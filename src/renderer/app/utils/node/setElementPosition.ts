5251b546196f2e73c90618e990034a0416f3adca
nametake <nametake.kyarabuki@gmail.com>
2022-01-28 00:18:52 +0900

    Moved expandNodes

--- src/renderer/app/utils/node/setElementPosition.ts
+++ src/renderer/app/utils/node/setElementPosition.ts
@@ -1,12 +1,51 @@
-import { Node } from '@/app/types';
+import {
+  CauseNodeType,
+  ElementNodeType,
+  Node,
+  ResultNodeType,
+} from '@/app/types';
 import { LengthType, parseLength } from '@/app/utils/css';
 
 import { alignVertical } from './alignVertical';
-import { CauseNodeWithElements, expandNodes, mergeNodes } from './expandNodes';
 
 type SetElementsDimentionOption = {
   elementGap?: LengthType;
 };
+
+type CauseNodeWithElements = CauseNodeType & {
+  elements: ElementNodeType[];
+};
+
+const toElementNode = (nodes: Node[], parentNode: Node): ElementNodeType[] =>
+  nodes
+    .filter((node): node is ElementNodeType => node.type === 'element')
+    .filter((node) => node.parentNode === parentNode.id);
+
+const toCauseNodeWithElements = (nodes: Node[]): CauseNodeWithElements[] =>
+  nodes
+    .filter((node): node is CauseNodeType => node.type === 'cause')
+    .map<CauseNodeWithElements>((node) => ({
+      ...node,
+      elements: toElementNode(nodes, node),
+    }));
+
+const toResultNode = (nodes: Node[]): ResultNodeType[] =>
+  nodes.filter((node): node is ResultNodeType => node.type === 'result');
+
+const expandNodes = (
+  nodes: Node[]
+): [CauseNodeWithElements[], ResultNodeType[]] => [
+    toCauseNodeWithElements(nodes),
+    toResultNode(nodes),
+  ];
+
+const mergeNodes = ([causeNodes, resultNodes]: [
+  CauseNodeWithElements[],
+  ResultNodeType[]
+]): Node[] => [
+    ...causeNodes.flatMap(({ elements, ...node }) => [node, ...elements]),
+    ...resultNodes,
+  ];
 
 // setElementsDimention sets elements dimention in CauseNode.
 //
