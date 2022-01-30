8ea66c408604d940d533b6326444ce7d96c32a68
nametake <nametake.kyarabuki@gmail.com>
2022-01-30 09:46:44 +0900

    Added merge test case

--- src/renderer/app/utils/node/__tests__/merge.test.ts
+++ src/renderer/app/utils/node/__tests__/merge.test.ts
@@ -40,4 +40,43 @@ describe('merge', () => {
 
     expect(merge({ oldNodes, newNodes })).toStrictEqual(expected);
   });
+
+  test('remove old nodes', () => {
+    const oldNodes: Node[] = [
+      {
+        id: 'old-id',
+        type: 'cause',
+        position: { x: 10, y: 10 },
+        data: { label: { text: 'old' } },
+        style: {
+          padding: '10px',
+        },
+      },
+    ];
+    const newNodes: Node[] = [
+      {
+        id: 'new-id',
+        type: 'cause',
+        position: { x: 0, y: 0 },
+        data: { label: { text: 'new' } },
+        style: {
+          padding: '20px',
+        },
+      },
+    ];
+
+    const expected: Node[] = [
+      {
+        id: 'new-id',
+        type: 'cause',
+        position: { x: 0, y: 0 },
+        data: { label: { text: 'new' } },
+        style: {
+          padding: '20px',
+        },
+      },
+    ];
+
+    expect(merge({ oldNodes, newNodes })).toStrictEqual(expected);
+  });
 });
