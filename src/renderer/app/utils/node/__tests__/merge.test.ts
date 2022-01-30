5f56a603286cc36e6050ecdcb1cb7d0ea4dbbdeb
nametake <nametake.kyarabuki@gmail.com>
2022-01-30 09:44:13 +0900

    Added node/merge test

---
+++ src/renderer/app/utils/node/__tests__/merge.test.ts
@@ -0,0 +1,43 @@
+import { merge } from '../merge';
+import { Node } from '@/app/types';
+
+describe('merge', () => {
+  test('keep old position', () => {
+    const oldNodes: Node[] = [
+      {
+        id: 'id',
+        type: 'cause',
+        position: { x: 10, y: 10 },
+        data: { label: { text: 'old' } },
+        style: {
+          padding: '10px',
+        },
+      },
+    ];
+    const newNodes: Node[] = [
+      {
+        id: 'id',
+        type: 'cause',
+        position: { x: 0, y: 0 },
+        data: { label: { text: 'new' } },
+        style: {
+          padding: '20px',
+        },
+      },
+    ];
+
+    const expected: Node[] = [
+      {
+        id: 'id',
+        type: 'cause',
+        position: { x: 10, y: 10 },
+        data: { label: { text: 'new' } },
+        style: {
+          padding: '20px',
+        },
+      },
+    ];
+
+    expect(merge({ oldNodes, newNodes })).toStrictEqual(expected);
+  });
+});
