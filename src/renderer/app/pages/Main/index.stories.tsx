4e2dfbfea2f127b1bc5e31106381627d413281cd
nametake <nametake.kyarabuki@gmail.com>
2022-02-04 01:15:21 +0900

    Fixed initial state

--- src/renderer/app/pages/Main/index.stories.tsx
+++ src/renderer/app/pages/Main/index.stories.tsx
@@ -5,10 +5,6 @@ import { ComponentMeta, ComponentStory } from '@storybook/react';
 /* eslint-enable */
 
 import { Node } from '@/app/types';
-import { causeNodeLabelStyle, causeNodeStyle } from '@/app/ui/CauseNode';
-import { elementNodeStyle } from '@/app/ui/ElementNode';
-import { resultNodeStyle } from '@/app/ui/ResultNode';
-import { Node as NodeUitls } from '@/app/utils/node';
 
 import { useMain } from './hooks';
 import { Main } from './Main';
@@ -36,56 +32,140 @@ Default.args = {};
 
 const initialNode: Node[] = [
   {
-    id: '1-1',
-    data: { label: { text: 'Card' } },
+    id: '1-2',
+    data: {
+      label: { text: 'Card', style: { height: 24 } },
+      elements: { width: 68, height: 100 },
+    },
     type: 'cause',
     position: { x: 0, y: 0 },
+    style: {
+      width: 'fit-content',
+      height: 'fit-content',
+      border: '1px solid',
+      paddingTop: 10,
+      paddingRight: 20,
+      paddingBottom: 10,
+      paddingLeft: 20,
+    },
+    width: 110,
+    height: 146,
   },
   {
-    id: '1-2',
+    id: '1-3',
     data: { label: 'Suica' },
-    parentNode: '1-1',
+    parentNode: '1-2',
     type: 'element',
     position: { x: 20, y: 44 },
+    style: {
+      borderRadius: '8px',
+      padding: 8,
+      border: '1px solid',
+      textAlign: 'center',
+      pointerEvents: 'none',
+    },
+    draggable: false,
+    width: 58,
+    height: 40,
   },
   {
-    id: '2-2',
+    id: '2-3',
     data: { label: 'Pasmo' },
-    parentNode: '1-1',
+    parentNode: '1-2',
     type: 'element',
     position: { x: 20, y: 94 },
-  },
-  {
-    id: '3-1',
-    data: { label: { text: 'Age' } },
-    type: 'cause',
-    position: { x: 193, y: 0 },
+    style: {
+      borderRadius: '8px',
+      padding: 8,
+      border: '1px solid',
+      textAlign: 'center',
+      pointerEvents: 'none',
+    },
+    draggable: false,
+    width: 68,
+    height: 40,
   },
   {
     id: '3-2',
+    data: {
+      label: { text: 'Age', style: { height: 24 } },
+      elements: { width: 84, height: 100 },
+    },
+    type: 'cause',
+    position: { x: 190, y: 0 },
+    style: {
+      width: 'fit-content',
+      height: 'fit-content',
+      border: '1px solid',
+      paddingTop: 10,
+      paddingRight: 20,
+      paddingBottom: 10,
+      paddingLeft: 20,
+    },
+    width: 126,
+    height: 146,
+  },
+  {
+    id: '3-3',
     data: { label: 'Over 60' },
-    parentNode: '3-1',
+    parentNode: '3-2',
     type: 'element',
     position: { x: 20, y: 44 },
+    style: {
+      borderRadius: '8px',
+      padding: 8,
+      border: '1px solid',
+      textAlign: 'center',
+      pointerEvents: 'none',
+    },
+    draggable: false,
+    width: 75,
+    height: 40,
   },
   {
-    id: '4-2',
+    id: '4-3',
     data: { label: 'Under 60' },
-    parentNode: '3-1',
+    parentNode: '3-2',
     type: 'element',
     position: { x: 20, y: 94 },
+    style: {
+      borderRadius: '8px',
+      padding: 8,
+      border: '1px solid',
+      textAlign: 'center',
+      pointerEvents: 'none',
+    },
+    draggable: false,
+    width: 84,
+    height: 40,
   },
   {
-    id: '6-2',
+    id: '6-3',
     data: { label: 'Yes' },
     type: 'result',
-    position: { x: 408, y: 0 },
+    position: { x: 396, y: 0 },
+    style: {
+      borderRadius: '50%',
+      padding: 16,
+      border: '1px solid',
+      textAlign: 'center',
+    },
+    width: 60,
+    height: 56,
   },
   {
-    id: '7-2',
+    id: '7-3',
     data: { label: 'No' },
     type: 'result',
-    position: { x: 408, y: 101 },
+    position: { x: 396, y: 96 },
+    style: {
+      borderRadius: '50%',
+      padding: 16,
+      border: '1px solid',
+      textAlign: 'center',
+    },
+    width: 54,
+    height: 56,
   },
 ];
 
@@ -94,49 +174,39 @@ const TemplateWithInitialState: ComponentStory<typeof Main> =
   function TemplateWithInitialState() {
     const { causeFlowProps, decisionTableProps } = useMain({
       initialState: {
-        nodes: initialNode.map(
-          NodeUitls.mapStyle({
-            causeNodeStyle,
-            causeNodeLabelStyle,
-            elementNodeStyle,
-            resultNodeStyle,
-          })
-        ),
+        nodes: initialNode,
         edges: [
           {
-            source: '1-1',
+            source: '1-2',
             sourceHandle: null,
-            target: '3-1',
+            target: '3-2',
             targetHandle: null,
-            id: 'reactflow__edge-1-1-3-1',
+            id: 'reactflow__edge-1-2-3-2',
             type: 'removable',
           },
           {
-            source: '3-2',
+            source: '3-3',
             sourceHandle: null,
-            target: '6-2',
+            target: '6-3',
             targetHandle: null,
-            id: 'reactflow__edge-3-2-6-2',
+            id: 'reactflow__edge-3-3-6-3',
             type: 'removable',
           },
           {
-            source: '4-2',
+            source: '4-3',
             sourceHandle: null,
-            target: '7-2',
+            target: '7-3',
             targetHandle: null,
-            id: 'reactflow__edge-4-2-7-2',
+            id: 'reactflow__edge-4-3-7-3',
             type: 'removable',
           },
         ],
         grid: [
           [
             { value: { type: 'ADD_CONDITION_ROW_BUTTON' } },
-            {
-              value: { type: 'CONDITION_HEADER', value: 'Condition' },
-            },
-            {
-              value: { type: 'CONDITION_HEADER', value: 'Condition stub' },
-            },
+            { value: { type: 'ROW_NUMBER' } },
+            { value: { type: 'CONDITION_HEADER', value: 'Condition' } },
+            { value: { type: 'CONDITION_HEADER', value: 'Condition stub' } },
             { value: { type: 'CONDITION_HEADER', value: '1' } },
             { value: { type: 'CONDITION_HEADER', value: '2' } },
             { value: { type: 'CONDITION_HEADER', value: '3' } },
@@ -144,6 +214,7 @@ const TemplateWithInitialState: ComponentStory<typeof Main> =
           ],
           [
             { value: { type: 'REMOVE_ROW' } },
+            { value: { type: 'ROW_NUMBER' } },
             { value: { type: 'CONDITION_NAME', value: 'Card' } },
             { value: { type: 'CONDITION_STUB', value: 'Suica' } },
             { value: { type: 'CONDITION_RULE', value: 'yes' } },
@@ -153,6 +224,7 @@ const TemplateWithInitialState: ComponentStory<typeof Main> =
           ],
           [
             { value: { type: 'REMOVE_ROW' } },
+            { value: { type: 'ROW_NUMBER' } },
             { value: { type: 'CONDITION_NAME', value: '' } },
             { value: { type: 'CONDITION_STUB', value: 'Pasmo' } },
             { value: { type: 'CONDITION_RULE', value: 'no' } },
@@ -162,6 +234,7 @@ const TemplateWithInitialState: ComponentStory<typeof Main> =
           ],
           [
             { value: { type: 'REMOVE_ROW' } },
+            { value: { type: 'ROW_NUMBER' } },
             { value: { type: 'CONDITION_NAME', value: 'Age' } },
             { value: { type: 'CONDITION_STUB', value: 'Over 60' } },
             { value: { type: 'CONDITION_RULE', value: 'yes' } },
@@ -171,6 +244,7 @@ const TemplateWithInitialState: ComponentStory<typeof Main> =
           ],
           [
             { value: { type: 'REMOVE_ROW' } },
+            { value: { type: 'ROW_NUMBER' } },
             { value: { type: 'CONDITION_NAME', value: '' } },
             { value: { type: 'CONDITION_STUB', value: 'Under 60' } },
             { value: { type: 'CONDITION_RULE', value: 'no' } },
@@ -180,19 +254,17 @@ const TemplateWithInitialState: ComponentStory<typeof Main> =
           ],
           [
             { value: { type: 'ADD_ACTION_ROW_BUTTON' } },
-            {
-              value: { type: 'ACTION_HEADER', value: 'Action' },
-            },
-            {
-              value: { type: 'ACTION_HEADER', value: 'Action stub' },
-            },
-            { value: { type: 'EMPTY' } },
-            { value: { type: 'EMPTY' } },
-            { value: { type: 'EMPTY' } },
-            { value: { type: 'EMPTY' } },
+            { value: { type: 'ROW_NUMBER' } },
+            { value: { type: 'ACTION_HEADER', value: 'Action' } },
+            { value: { type: 'ACTION_HEADER', value: 'Action stub' } },
+            { value: { type: 'ACTION_HEADER', value: null } },
+            { value: { type: 'ACTION_HEADER', value: null } },
+            { value: { type: 'ACTION_HEADER', value: null } },
+            { value: { type: 'ACTION_HEADER', value: null } },
           ],
           [
             { value: { type: 'REMOVE_ROW' } },
+            { value: { type: 'ROW_NUMBER' } },
             { value: { type: 'ACTION_NAME', value: 'Discount' } },
             { value: { type: 'ACTION_STUB', value: 'Yes' } },
             { value: { type: 'CONDITION_RULE', value: 'yes' } },
@@ -202,7 +274,8 @@ const TemplateWithInitialState: ComponentStory<typeof Main> =
           ],
           [
             { value: { type: 'REMOVE_ROW' } },
-            { value: { type: 'ACTION_NAME', value: '' } },
+            { value: { type: 'ROW_NUMBER' } },
+            { value: { type: 'ACTION_NAME', value: null } },
             { value: { type: 'ACTION_STUB', value: 'No' } },
             { value: { type: 'CONDITION_RULE', value: 'no' } },
             { value: { type: 'CONDITION_RULE', value: 'yes' } },
@@ -219,6 +292,7 @@ const TemplateWithInitialState: ComponentStory<typeof Main> =
       />
     );
   };
-/* eslint-enable */ export const WithInitialState =
-  TemplateWithInitialState.bind({});
+
+/* eslint-enable */
+export const WithInitialState = TemplateWithInitialState.bind({});
 WithInitialState.args = {};
