503bedbb767b25f26978e8f5521bf99570ff4f0f
nametake <nametake.kyarabuki@gmail.com>
2022-02-07 00:51:00 +0900

    Added empty stub row

--- src/renderer/app/utils/grid/mergeRules.ts
+++ src/renderer/app/utils/grid/mergeRules.ts
@@ -1,10 +1,43 @@
 import { Action, CellType, Condition, Rule } from '@/app/types';
-
-import { findActionRow } from './findActionRow';
+import { assertUnreachable } from '@/app/utils/assert';
 
 type MergeRulesOption = {
   nameColumn: number;
   stubColumn: number;
+};
+
+type RowType =
+  | 'CONDITION_HEADER'
+  | 'ACTION_HEADER'
+  | 'CONDITION_ROW'
+  | 'ACTION_ROW'
+  | 'EMPTY_STUB_ROW';
+
+const getRowType = (
+  row: CellType[],
+  rowIndex: number,
+  option: MergeRulesOption
+): RowType => {
+  const { nameColumn, stubColumn } = option;
+  if (rowIndex === 0 && row[nameColumn].value.type === 'CONDITION_HEADER') {
+    return 'CONDITION_HEADER';
+  }
+
+  if (rowIndex !== 0 && row[nameColumn].value.type === 'ACTION_HEADER') {
+    return 'ACTION_HEADER';
+  }
+
+  const { value } = row[stubColumn];
+
+  if (value.type === 'CONDITION_STUB' && value.value) {
+    return 'CONDITION_ROW';
+  }
+
+  if (value.type === 'ACTION_STUB' && value.value) {
+    return 'ACTION_ROW';
+  }
+
+  return 'EMPTY_STUB_ROW';
 };
 
 export const mergeRules = (
@@ -14,85 +47,73 @@ export const mergeRules = (
   rules: Rule[],
   option: MergeRulesOption
 ): CellType[][] => {
-  const { nameColumn, stubColumn } = option;
+  const { stubColumn } = option;
 
   const conditionStubs = conditions.flatMap((condition) => condition.stub);
   const actionStubs = actions.flatMap((action) => action.stub);
 
   const newGrid = grid.map((row) => row.slice(0, stubColumn + 1));
 
-  const actionHeaderRow = findActionRow(newGrid);
-
   rules.forEach((rule, ruleIndex) => {
     newGrid.forEach((row, rowIndex) => {
-      const isConditionHeader =
-        rowIndex === 0 && row[nameColumn].value.type === 'CONDITION_HEADER';
-      const isActionHeader =
-        rowIndex !== 0 && row[nameColumn].value.type === 'ACTION_HEADER';
-
-      // --- Condition header ---
-      if (isConditionHeader) {
-        newGrid[rowIndex] = [
-          ...row,
-          { value: { type: 'CONDITION_HEADER', value: `${ruleIndex + 1}` } },
-        ];
-        return;
-      }
-
-      // --- Action header ---
-      if (isActionHeader) {
-        newGrid[rowIndex] = [
-          ...row,
-          { value: { type: 'ACTION_HEADER', value: null } },
-        ];
-        return;
-      }
-
-      // --- Stub cell empty ---
-      const stubCell = row[stubColumn];
-      if (
-        (stubCell.value.type === 'CONDITION_STUB' ||
-          stubCell.value.type === 'ACTION_STUB') &&
-        !stubCell.value.value
-      ) {
-        newGrid[rowIndex] = [...row, { value: { type: 'EMPTY' } }];
-        return;
-      }
-
-      // --- Condition rule ---
-      if (actionHeaderRow > rowIndex) {
-        const conditionStub = conditionStubs.find(
-          (c) => c.location.row === rowIndex
-        );
-        if (rule.conditionStubIds.find((id) => id === conditionStub?.id)) {
+      const type = getRowType(row, rowIndex, option);
+      switch (type) {
+        case 'CONDITION_HEADER': {
           newGrid[rowIndex] = [
             ...row,
-            { value: { type: 'CONDITION_RULE', value: 'yes' } },
+            { value: { type: 'CONDITION_HEADER', value: `${ruleIndex + 1}` } },
           ];
           return;
         }
-        newGrid[rowIndex] = [
-          ...row,
-          { value: { type: 'CONDITION_RULE', value: 'no' } },
-        ];
-        return;
+        case 'ACTION_HEADER': {
+          newGrid[rowIndex] = [
+            ...row,
+            { value: { type: 'ACTION_HEADER', value: null } },
+          ];
+          return;
+        }
+        case 'CONDITION_ROW': {
+          const conditionStub = conditionStubs.find(
+            (c) => c.location.row === rowIndex
+          );
+          if (rule.conditionStubIds.find((id) => id === conditionStub?.id)) {
+            newGrid[rowIndex] = [
+              ...row,
+              { value: { type: 'CONDITION_RULE', value: 'yes' } },
+            ];
+            return;
+          }
+          newGrid[rowIndex] = [
+            ...row,
+            { value: { type: 'CONDITION_RULE', value: 'no' } },
+          ];
+          return;
+        }
+        case 'ACTION_ROW': {
+          const actionStub = actionStubs.find(
+            (stub) => stub.location.row === rowIndex
+          );
+          if (rule.actionId === actionStub?.id) {
+            newGrid[rowIndex] = [
+              ...row,
+              { value: { type: 'CONDITION_RULE', value: 'yes' } },
+            ];
+            return;
+          }
+          newGrid[rowIndex] = [
+            ...row,
+            { value: { type: 'CONDITION_RULE', value: 'no' } },
+          ];
+          return;
+        }
+        case 'EMPTY_STUB_ROW': {
+          newGrid[rowIndex] = [...row, { value: { type: 'EMPTY' } }];
+          break;
+        }
+        default:
+          assertUnreachable(type);
+          
       }
-
-      // --- Action rule ---
-      const actionStub = actionStubs.find(
-        (stub) => stub.location.row === rowIndex
-      );
-      if (rule.actionId === actionStub?.id) {
-        newGrid[rowIndex] = [
-          ...row,
-          { value: { type: 'CONDITION_RULE', value: 'yes' } },
-        ];
-        return;
-      }
-      newGrid[rowIndex] = [
-        ...row,
-        { value: { type: 'CONDITION_RULE', value: 'no' } },
-      ];
     });
   });
 
