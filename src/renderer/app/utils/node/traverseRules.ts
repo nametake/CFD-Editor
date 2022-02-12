6db759004abdb2213d7bcded80175284ead8e0ee
nametake <nametake.kyarabuki@gmail.com>
2022-02-13 00:19:09 +0900

    Merge same conditionStubIds rule

--- src/renderer/app/utils/node/traverseRules.ts
+++ src/renderer/app/utils/node/traverseRules.ts
@@ -34,7 +34,9 @@ const traverse = (
       const connectedEdges = edges.filter(
         (edge) => edge.source === currentNode.id
       );
-      const nextNodes = nodes.filter((node) => connectedEdges.find((edge) => node.id === edge.target));
+      const nextNodes = nodes.filter((node) =>
+        connectedEdges.find((edge) => node.id === edge.target)
+      );
       const nextRule: Rule = {
         ...currentRule,
         conditionStubIds: [...currentRule.conditionStubIds, currentNode.id],
@@ -58,12 +60,33 @@ const traverse = (
 
 export const traverseRules = (nodes: Node[], edges: Edge[]): Rule[] => {
   const startNode = nodes.find((node) => node.type === 'cause');
-  return startNode
-    ? traverse(
-      { conditionStubIds: [], actionStubIds: [] },
-      startNode,
-      nodes,
-      edges
-    )
-    : [];
+  if (!startNode) {
+    return [];
+  }
+  const rules = traverse(
+    { conditionStubIds: [], actionStubIds: [] },
+    startNode,
+    nodes,
+    edges
+  );
+
+  const map = new Map<string, Rule[]>();
+
+  rules.forEach((rule) => {
+    const prevRules = map.get(JSON.stringify(rule.conditionStubIds));
+    map.set(JSON.stringify(rule.conditionStubIds), [
+      ...(prevRules ?? []),
+      rule,
+    ]);
+  });
+
+  return [...map.values()].map((value) => value.reduce((prev, rule) => {
+      if (!prev) {
+        return rule;
+      }
+      return {
+        ...prev,
+        actionStubIds: [...prev.actionStubIds, ...rule.actionStubIds],
+      };
+    }), null);
 };
