84c1a8c0e6da1db46a9a866c36fd68922207a94a
nametake <nametake.kyarabuki@gmail.com>
2022-02-23 18:58:25 +0900

    Implemented invalidFilerRules

--- src/renderer/app/utils/rules/filterInvalidRules.ts
+++ src/renderer/app/utils/rules/filterInvalidRules.ts
@@ -2,6 +2,39 @@ import { Action, Condition, Rule } from '@/app/types';
 
 export const filterInvalidRules = (
   rules: Rule[],
-  _conditions: Condition[],
-  _actions: Action[]
-) => rules;
+  conditions: Condition[],
+  actions: Action[]
+) => {
+  const invalidConditionStubIds = conditions.flatMap((condition) =>
+    condition.stub.filter((s) => s.type === 'invalid').map((s) => s.id)
+  );
+  const invalidAcitonStubIds = actions.flatMap((action) =>
+    action.stub.filter((s) => s.type === 'invalid').map((s) => s.id)
+  );
+
+  const set = new Set<string>();
+
+  return rules.reduce<Rule[]>((prev, rule) => {
+    const isAllInvalidIds = rule.actionStubIds.every((ruleActionId) =>
+      invalidAcitonStubIds.find((id) => id === ruleActionId)
+    );
+    if (!isAllInvalidIds) {
+      return [...prev, rule];
+    }
+
+    const invalidIds = rule.conditionStubIds.filter((id) =>
+      invalidConditionStubIds.find((i) => i === id)
+    );
+    if (invalidIds.length === 0) {
+      return [...prev, rule];
+    }
+
+    const exist = invalidIds.find((id) => !set.has(id));
+    if (exist === undefined) {
+      return prev;
+    }
+
+    set.add(exist);
+    return [...prev, rule];
+  }, []);
+};
