177d673022c9d500f7890b13f23ea1e921abd485
nametake <nametake.kyarabuki@gmail.com>
2022-03-19 23:23:20 +0900

    Removed unused file

--- src/renderer/app/hooks/useQueryStringReducer/index.ts
+++ src/renderer/app/hooks/useQueryStringReducer/index.ts
@@ -9,17 +9,18 @@ type QueryStringIO<S> = {
 export function useQueryStringReducer<S, A>(
   reducer: Reducer<S, A>,
   initialState: S,
-  io: QueryStringIO<S>,
+  io: QueryStringIO<S>
 ): [S, Dispatch<A>] {
   const [searchParams, setSearchParams] = useSearchParams();
-  const [state, setState] = useState([...searchParams.keys()].length !== 0 ? io.from(searchParams) : initialState);
-
+  const [state, setState] = useState(
+    [...searchParams.keys()].length !== 0 ? io.from(searchParams) : initialState
+  );
 
   const dispatch = useCallback(
     (action: A) => {
-      const next = reducer(state, action)
-      setSearchParams(io.to(next))
-      setState(next)
+      const next = reducer(state, action);
+      setSearchParams(io.to(next));
+      setState(next);
     },
     [reducer, io, state, setState, setSearchParams]
   );
