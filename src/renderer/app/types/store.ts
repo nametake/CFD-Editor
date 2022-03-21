c2cee39a769b9e8278f90667b642b2f751f5efd8
nametake <nametake.kyarabuki@gmail.com>
2022-03-22 00:32:27 +0900

    Added store utils

--- src/renderer/app/types/store.ts
+++ src/renderer/app/types/store.ts
@@ -1,16 +1,26 @@
+import { ValueType } from './cell';
+import { EdgeType, NodeType } from './node';
+
 export type StoreNode = {
   id?: string;
+  type?: NodeType;
+  label?: string;
   position?: { x?: number; y?: number };
 };
 
 export type StoreEdge = {
   id?: string;
+  type?: EdgeType;
   source?: string;
   target?: string;
 };
 
 export type StoreCell = {
-  x?: number;
-  y?: number;
-  value?: string;
+  value?: ValueType;
+};
+
+export type StoreModel = {
+  nodes: StoreNode[];
+  edges: StoreEdge[];
+  grid: StoreCell[][];
 };
---
+++ src/renderer/app/utils/store/index.ts
@@ -0,0 +1,62 @@
+import { CellType, Edge, Node } from '@/app/types';
+import { StoreCell, StoreEdge, StoreModel, StoreNode } from '@/app/types/store';
+import { assertUnreachable, throwError } from '@/app/utils/assert';
+
+export type ToArgs = {
+  nodes: Node[];
+  edges: Edge[];
+  grid: CellType[][];
+};
+
+export const from = ({ nodes, edges, grid }: ToArgs): StoreModel => ({
+    nodes: nodes.map<StoreNode>((node) => ({ id: node.id })),
+    edges: edges.map<StoreEdge>((edge) => ({ id: edge.id })),
+    grid: grid.map<StoreCell[]>((row) =>
+      row.map<StoreCell>((cell) => ({ value: cell.value }))
+    ),
+  });
+
+export const to = ({
+  nodes,
+  edges,
+  grid,
+}: StoreModel): { nodes: Node[]; edges: Edge[]; grid: CellType[][] } => ({
+    nodes: nodes.map<Node>((node): Node => {
+      const n: Omit<Node, 'type' | 'data'> = {
+        id: node.id ?? throwError(`node: not found 'id'`),
+        position: { x: node.position?.x ?? 0, y: node.position?.y ?? 0 },
+      };
+      switch (node.type) {
+        case 'cause':
+          return {
+            ...n,
+            type: node.type,
+            data: { label: { text: node.label ?? '' } },
+          };
+        case 'element':
+        case 'result':
+          return {
+            ...n,
+            type: node.type,
+            data: { label: node.label ?? '' },
+          };
+        case undefined:
+          return throwError(`node: not found 'type'`);
+        default:
+          return assertUnreachable(node.type);
+      }
+    }),
+    edges: edges.map<Edge>(
+      (edge): Edge => ({
+        id: edge.id ?? throwError(`edge: not found id`),
+        type: edge.type ?? throwError(`edge: not found type`),
+        source: edge.source ?? throwError(`edge: not found source`),
+        target: edge.target ?? throwError(`edge: not found target`),
+      })
+    ),
+    grid: grid.map<CellType[]>((row) =>
+      row.map<CellType>((cell) => ({
+        value: cell.value ?? throwError(`cell: not found value`),
+      }))
+    ),
+  });
